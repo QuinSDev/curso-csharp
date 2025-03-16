199e39756dab005c6b290aef883c07bd97570477
QuinSDev <quindev97@gmail.com>
2025-03-15 19:10:22 -0500

    Exercise one, creation of a program that determines wheter a number is even or odd

---
+++ TallerProgramacion/NumeroParImpar/Program.cs
@@ -0,0 +1,17 @@
+﻿//Construir un programa que pida un número y luego diga si este número es par o no.
+
+do
+{
+    Console.Write("Ingresa un número: ");
+    var numero = int.Parse(Console.ReadLine()!);
+
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine("El número: " + numero + ", es par\n");
+    }
+    else
+    {
+        Console.WriteLine("El número: " + numero + ", es impar\n");
+    }
+}
+while (true);
