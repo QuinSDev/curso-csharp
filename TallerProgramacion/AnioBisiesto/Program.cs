d89587627e6e5bc37a23ffd2dc1f94164d7dcaed
QuinSDev <quindev97@gmail.com>
2025-03-21 18:24:29 -0500

    Se desarrolla un programa que determina si un año es bisiesto o no

---
+++ TallerProgramacion/AnioBisiesto/Program.cs
@@ -0,0 +1,24 @@
+﻿// Construir un programa que pida un año y luego determine si el año es bisiesto o no.
+// Tenga en cuenta que los años bisiestos son los números múltiplos de 4,
+// pero que no son múltiplos de 100 y si son múltiplos de 100 y múltiplos de 400 sí son bisiestos.
+
+Console.Write("Ingrese año: ");
+var anio = Console.ReadLine();
+
+if (int.TryParse(anio, out int intAnio)) {
+
+    if (intAnio % 4 == 0 && intAnio % 100 !=0 || intAnio % 400 == 0)
+    {
+        Console.WriteLine($"El año {intAnio}, es bisiesto.");
+    }
+    else
+    {
+        Console.WriteLine($"El año {intAnio}, no es bisiesto.");
+    }
+
+}
+else
+{
+    Console.WriteLine("Ingrese los vaolores numéricos del año. No letras");
+}
+
